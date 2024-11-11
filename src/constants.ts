44b53209de74acc28e4df82d811b6d94358c9ac0
BoxBoxJason <loickgoupil@yahoo.fr>
2024-11-11 15:21:14 +0100

    Replace Python achievements builder with typescript Goal is to not use too many different languages

---
+++ src/constants.ts
@@ -0,0 +1,93 @@
+/**
+ * constants module stores the shared constants for the achievements builder
+ */
+
+export namespace constants {
+    //////////////////////// CATEGORIES ////////////////////////
+    export const category = {
+      FILES: 'files',
+      GIT: 'git',
+      PRODUCTIVITY: 'productivity',
+      VSCODE: 'vscode'
+    } as const;
+
+    //////////////////////// CRITERIAS ////////////////////////
+    export const criteria = {
+      LINES_OF_CODE: 'linesOfCodeCount',
+      LINES_OF_CODE_LANGUAGE: 'linesOfCodeCount_%s',
+      LINES_OF_COMMENTS: 'linesOfCommentsCount',
+      NUMBER_OF_PASTES: 'pastesCount',
+      ERRORS_FIXED: 'errorsFixedCount',
+      FILES_CREATED: 'filesCreatedCount',
+      FILES_CREATED_LANGUAGE: 'filesCreatedCount_%s',
+      FILES_DELETED: 'filesDeletedCount',
+      FILES_MOVED: 'filesMovedCount',
+      COMMITS: 'commitsCount',
+      BRANCHES_CREATED: 'branchesCreatedCount',
+      MERGES_AND_REBASES: 'mergesAndRebasesCount',
+      AMENDS: 'amendsCount',
+      FORCED_PUSHES: 'forcedPushesCount',
+      PUSHES: 'pushesCount',
+      NUMBER_OF_SIMULTANEOUS_TABS: 'SimultaneousTabsCount',
+      TIME_SPENT: 'timeSpentCount',
+      EXTENSIONS_INSTALLED: 'extensionsInstalledCount',
+      EXTENSIONS_OUTDATED: 'extensionsOutdatedCount',
+      THEMES_INSTALLED: 'themesInstalledCount'
+    } as const;
+
+    //////////////////////// LABELS ////////////////////////
+    export const labels = {
+      LINES_OF_CODE: 'lines of code',
+      LINES_OF_COMMENTS: 'lines of comments',
+      NUMBER_OF_PASTES: 'number of pastes',
+      ERRORS_FIXED: 'errors fixed',
+      FILES_CREATED: 'files created',
+      FILES_DELETED: 'files deleted',
+      FILES_MOVED: 'files moved',
+      COMMITS: 'commits',
+      BRANCHES_CREATED: 'branches created',
+      MERGES_AND_REBASES: 'merges and rebases',
+      AMENDS: 'amends',
+      FORCED_PUSHES: 'forced pushes',
+      PUSHES: 'pushes',
+      NUMBER_OF_SIMULTANEOUS_TABS: 'number of simultaneous tabs',
+      TIME_SPENT: 'time spent',
+      EXTENSIONS_INSTALLED: 'extensions installed',
+      EXTENSIONS_OUTDATED: 'extensions outdated',
+      THEMES_INSTALLED: 'themes installed',
+      LANGUAGES: [
+        'java',
+        'python',
+        'javascript',
+        'typescript',
+        'ada',
+        'c',
+        'c++',
+        'c#',
+        'ruby',
+        'php',
+        'swift',
+        'kotlin',
+        'go',
+        'rust',
+        'html',
+        'css',
+        'scss',
+        'less',
+        'sql',
+        'shell',
+        'bash',
+        'powershell',
+        'cobol',
+        'fortran',
+        'pascal',
+        'perl',
+        'lua',
+        'matlab',
+        'objective-c',
+        'scala',
+        'haskell',
+        'r'
+      ] as const
+    } as const;
+  }
