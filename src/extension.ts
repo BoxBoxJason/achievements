86697832b833d9f3cfb3b40328e2339f0fcac3c3
BoxBoxJason <loickgoupil@yahoo.fr>
2024-11-17 00:25:56 +0100

    Add extensions commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,57 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { db_model } from './database/model/model';
+import logger from './logger/logger';
+import { config } from './config/config';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	// ==================== CONFIG ====================
+	config.activate(context);
+	let configuration = config.getConfig();
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "achievements" is now active!');
+	// ==================== LOGGER ====================
+	logger.setLogDir(configuration.logDirectory);
+	logger.setLogLevel(configuration.logLevel);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('achievements.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Achievements!');
+	// ==================== DATABASE ====================
+	if (configuration.enabled) {
+		db_model.activate(context);
+	}
+
+	// ==================== COMMANDS ====================
+	// Warning, do not forget to add each command to the package.json file AND to the subscriptions array
+
+	// Enable command
+	const enable_command = vscode.commands.registerCommand('achievements.enable', () => {
+		config.enableExtension();
+
 	});
+	context.subscriptions.push(enable_command);
 
-	context.subscriptions.push(disposable);
+	// Disable command
+	const disable_command = vscode.commands.registerCommand('achievements.disable', () => {
+		config.enableExtension(false);
+		logger.info('Achievement disabled!');
+	});
+	context.subscriptions.push(disable_command);
+
+	// Configuration command
+	const configuration_command = vscode.commands.registerCommand('achievements.configuration', () => {
+		config.openConfiguration();
+	});
+	context.subscriptions.push(configuration_command);
+
+	// Show achievements command
+	const show_achievements_command = vscode.commands.registerCommand('achievements.show', () => {
+		logger.error('Not implemented yet');
+	});
+	context.subscriptions.push(show_achievements_command);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	db_model.deactivate();
+}
