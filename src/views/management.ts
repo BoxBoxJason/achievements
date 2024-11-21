4366c4aeb75b8b8df84df9c8c49067a570644527
BoxBoxJason <loickgoupil@yahoo.fr>
2024-11-21 23:34:17 +0100

    Move communication / requests to proper spots

--- src/views/management.ts
+++ src/views/management.ts
@@ -1,16 +1,18 @@
 import path from 'path';
 import * as vscode from 'vscode';
-import { getPackagedImages } from './request';
+import { getPackagedImages, PostMessage } from './request';
+import { backendRequests } from './requests/backend';
+import logger from '../utils/logger';
 
 /**
  * Setup methods for the Achievements webview panel
  */
 export namespace AchievementsWebview {
 
-  export function setupAchievementsPanel(context : vscode.ExtensionContext): vscode.WebviewPanel {
+  export function setupAchievementsPanel(context: vscode.ExtensionContext): vscode.WebviewPanel {
 
     // Create and show the webview panel
-    let panel : vscode.WebviewPanel | undefined = vscode.window.createWebviewPanel(
+    let panel: vscode.WebviewPanel | undefined = vscode.window.createWebviewPanel(
       'achievements',
       'Achievements',
       vscode.ViewColumn.One,
@@ -21,15 +23,29 @@ export namespace AchievementsWebview {
     );
     panel.webview.html = getDefaultWebviewContentReact(context, panel);
 
+    // Receive messages from the webview event
+    panel.webview.onDidReceiveMessage((message) => {
+      try {
+        const parsedMessage = JSON.parse(message) as PostMessage;
+
+        backendRequests.handleMessage(parsedMessage, panel);
+      } catch (e) {
+        logger.error('Cannot parse message: ' + e);
+      }
+    },
+      undefined,
+      context.subscriptions
+    );
+
     return panel;
   }
 
-  function getDefaultWebviewContentReact(context : vscode.ExtensionContext, panel : vscode.WebviewPanel): string {
+  function getDefaultWebviewContentReact(context: vscode.ExtensionContext, panel: vscode.WebviewPanel): string {
     let reactScriptUri = panel.webview.asWebviewUri(
-      vscode.Uri.file(path.join(__dirname,'webview.js'))
+      vscode.Uri.file(path.join(__dirname, 'webview.js'))
     );
     let cssUri = panel.webview.asWebviewUri(
-      vscode.Uri.file(path.join(path.dirname(__dirname),'src','views','styles','containers.css'))
+      vscode.Uri.file(path.join(path.dirname(__dirname), 'src', 'views', 'styles', 'containers.css'))
     );
     return `<!DOCTYPE html>
 <html lang="en">
