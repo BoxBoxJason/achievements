5eaa04f7f748ace28c3a061a051d0fee7739a221
BoxBoxJason <contact@boxboxjason.dev>
2024-11-21 23:34:17 +0100

    refactor: move communication / requests to proper spots

---
+++ src/views/requests/backend.ts
@@ -0,0 +1,28 @@
+import { AchievementController } from "../../database/controller/achievements";
+import { AchievementSelectRequestFilters } from "../../database/model/tables/Achievement";
+import { PostMessage } from "../request";
+import { webview } from "../viewconst";
+import * as vscode from 'vscode';
+
+export namespace backendRequests {
+
+  export function handleMessage(message: PostMessage, panel: vscode.WebviewPanel): void {
+    switch (message.command) {
+      case webview.commands.RETRIEVE_ACHIEVEMENTS:
+        handleAchievementsSelect(message.data, panel);
+        break;
+      default:
+        console.error('Unknown command: ' + message.command);
+    }
+  }
+
+  export function handleAchievementsSelect(filters: AchievementSelectRequestFilters | null, panel: vscode.WebviewPanel): void {
+    if (filters) {
+      const achievements = AchievementController.getAchievements(filters);
+      panel.webview.postMessage({ command: webview.commands.DISPLAY_ACHIEVEMENTS, data: achievements });
+    } else {
+      return;
+    }
+  }
+
+}
