57b5a90fb84a609631e0abba307e2c5e5966f9e6
BoxBoxJason <contact@boxboxjason.dev>
2024-12-08 23:44:29 +0100

    perf: cleanup db init to iterate over StackingTemplates

--- src/database/model/init/init.ts
+++ src/database/model/init/init.ts
@@ -40,96 +40,53 @@ export namespace db_init {
     logger.debug('Creating achievements from stacking templates');
 
     //////////////////////// PRODUCTIVITY ////////////////////////
-    // Lines of Code
-    logger.debug('Creating lines of code achievements');
-    Achievement.fromStackingTemplateToDB(StackingTemplates.productivity.linesOfCodeTemplate());
+    Object.entries(StackingTemplates.productivity).forEach(([name, func]) => {
+      if (typeof func === 'function') {
+        logger.debug(`Creating ${name} achievements`);
 
-    // Lines of Code for each language
-    for (const template of StackingTemplates.productivity.linesOfCodeLanguageTemplates()) {
-      logger.debug(`Creating lines of code achievements for ${template.title}`);
-      Achievement.fromStackingTemplateToDB(template);
-    }
-
-    // Lines of Comments
-    logger.debug('Creating lines of comments achievements');
-    Achievement.fromStackingTemplateToDB(StackingTemplates.productivity.linesOfCommentTemplate());
-
-    // Number of Pastes
-    logger.debug('Creating number of pastes achievements');
-    Achievement.fromStackingTemplateToDB(StackingTemplates.productivity.numberOfPastesTemplate());
-
-    // Errors Fixed
-    logger.debug('Creating errors fixed achievements');
-    Achievement.fromStackingTemplateToDB(StackingTemplates.productivity.errorsFixedTemplate());
-
-    // Time Spent Coding
-    logger.debug('Creating time spent coding achievements');
-    Achievement.fromStackingTemplateToDB(StackingTemplates.productivity.totalTimeSpentTemplate(), 3600);
-
-    // Number of Simultaneous Tabs
-    logger.debug('Creating number of simultaneous tabs achievements');
-    Achievement.fromStackingTemplateToDB(StackingTemplates.productivity.numberOfSimultaneousTabsTemplate());
-
-    //////////////////////// FILES ////////////////////////
-    // Files Created
-    logger.debug('Creating files created achievements');
-    Achievement.fromStackingTemplateToDB(StackingTemplates.files.filesCreatedTemplate());
-
-    // Files Created for Each Language
-    for (const template of StackingTemplates.files.filesCreatedLanguageTemplates()) {
-      logger.debug(`Creating files created achievements for ${template.title}`);
-      Achievement.fromStackingTemplateToDB(template);
-    }
-
-    // Directories Created
-    logger.debug('Creating directories created achievements');
-    Achievement.fromStackingTemplateToDB(StackingTemplates.files.directoriesCreatedTemplate());
-
-    // Files Renamed
-    logger.debug('Creating files renamed achievements');
-    Achievement.fromStackingTemplateToDB(StackingTemplates.files.filesRenamedCountTemplate());
-
-    // Resources Deleted
-    logger.debug('Creating resources deleted achievements');
-    Achievement.fromStackingTemplateToDB(StackingTemplates.files.resourceDeletedTemplate());
+        const achievements = func();
+        if (Array.isArray(achievements)) {
+          for (const achievement of achievements) {
+            Achievement.fromStackingTemplateToDB(achievement);
+          }
+        } else if (achievements.group === 'time spent'){
+          Achievement.fromStackingTemplateToDB(achievements, 3600);
+        } else {
+          Achievement.fromStackingTemplateToDB(achievements);
+        }
+      }
+    });
 
     //////////////////////// GIT ////////////////////////
-    // Commits
-    logger.debug('Creating commits achievements');
-    Achievement.fromStackingTemplateToDB(StackingTemplates.git.commitsTemplate());
+    Object.entries(StackingTemplates.git).forEach(([name, func]) => {
+      if (typeof func === 'function') {
+        logger.debug(`Creating ${name} achievements`);
+        Achievement.fromStackingTemplateToDB(func());
+      }
+    });
 
-    // Branches Created
-    logger.debug('Creating branches created achievements');
-    Achievement.fromStackingTemplateToDB(StackingTemplates.git.branchesCreatedTemplate());
+    //////////////////////// VS CODE //////////////////
+    Object.entries(StackingTemplates.vscode).forEach(([name, func]) => {
+      if (typeof func === 'function') {
+        logger.debug(`Creating ${name} achievements`);
+        Achievement.fromStackingTemplateToDB(func());
+      }
+    });
 
-    // Merges and Rebases
-    logger.debug('Creating merges and rebases achievements');
-    Achievement.fromStackingTemplateToDB(StackingTemplates.git.mergesAndRebasesTemplate());
-
-    // Amends
-    logger.debug('Creating amends achievements');
-    Achievement.fromStackingTemplateToDB(StackingTemplates.git.amendsTemplate());
-
-    // // Forced Pushes
-    // logger.debug('Creating forced pushes achievements');
-    // Achievement.fromStackingTemplateToDB(StackingTemplates.git.forcedPushesTemplate());
-
-    // Pushes
-    logger.debug('Creating pushes achievements');
-    Achievement.fromStackingTemplateToDB(StackingTemplates.git.pushesTemplate());
-
-    //////////////////////// VSCODE ////////////////////////
-    // Extensions Installed
-    logger.debug('Creating extensions installed achievements');
-    Achievement.fromStackingTemplateToDB(StackingTemplates.vscode.extensionsInstalledTemplate());
-
-    // Extensions Outdated
-    logger.debug('Creating extensions outdated achievements');
-    Achievement.fromStackingTemplateToDB(StackingTemplates.vscode.extensionsOutdatedTemplate());
-
-    // Themes Installed
-    logger.debug('Creating themes installed achievements');
-    Achievement.fromStackingTemplateToDB(StackingTemplates.vscode.themesInstalledTemplate());
+    //////////////////////// FILES ////////////////////////
+    Object.entries(StackingTemplates.files).forEach(([name, func]) => {
+      if (typeof func === 'function') {
+        logger.debug(`Creating ${name} achievements`);
+        const achievements = func();
+        if (Array.isArray(achievements)) {
+          for (const achievement of achievements) {
+            Achievement.fromStackingTemplateToDB(achievement);
+          }
+        } else {
+          Achievement.fromStackingTemplateToDB(achievements);
+        }
+      }
+    });
   }
 
   /**
