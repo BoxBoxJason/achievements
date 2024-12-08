3d8919ae53bdbc33907ce4251aa7d1494dcccd41
BoxBoxJason <contact@boxboxjason.dev>
2024-12-08 23:44:05 +0100

    feat: add tab events listeners

---
+++ src/listeners/tabs.ts
@@ -0,0 +1,21 @@
+/**
+ * Tab events listeners for achievements extension
+ *
+ * @namespace tabListeners
+ * @author BoxBoxJason
+ */
+
+import * as vscode from 'vscode';
+import { ProgressionController } from '../database/controller/progressions';
+import { constants } from '../constants';
+
+export namespace tabListeners {
+
+  export function activate(context: vscode.ExtensionContext): void {
+    vscode.window.tabGroups.onDidChangeTabs(() => {
+      const tabCount = vscode.window.tabGroups.all.reduce((count, group) => count + group.tabs.length, 0);
+      ProgressionController.updateProgression(constants.criteria.NUMBER_OF_SIMULTANEOUS_TABS, tabCount);
+    }, null, context.subscriptions);
+  }
+
+}
